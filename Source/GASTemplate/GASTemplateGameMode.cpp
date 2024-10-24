a7f5d0131369b8aa56f38cd45400ca1576832a58
Guybrush Threepwood <guibrush001@gmail.com>
2024-10-24 17:21:50 +0100

    UI for player and enemy working and tied to attributes

--- Source/GASTemplate/GASTemplateGameMode.cpp
+++ Source/GASTemplate/GASTemplateGameMode.cpp
@@ -7,7 +7,7 @@
 AGASTemplateGameMode::AGASTemplateGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/GASTemplate/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/GASTemplate/Blueprints/BP_PlayerCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
